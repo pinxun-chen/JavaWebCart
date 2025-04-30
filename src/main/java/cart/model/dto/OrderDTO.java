e01cef6e82d4a456b6cf44e07ed3b7173fc3bf3d
pinxun-chen <a930406a@gmail.com>
2025-04-30 11:32:24 +0800

    0430

---
+++ src/main/java/cart/model/dto/OrderDTO.java
@@ -0,0 +1,16 @@
+package cart.model.dto;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class OrderDTO {
+	private Integer orderId;
+	private Integer userId;
+	private Date orderDate;
+	// 集合該筆訂單所有的訂單項目
+	private List<OrderItemDTO> item = new ArrayList<>();
+}
